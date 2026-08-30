4e67965940cb2b607f96a94d1a7cb609e7a99e84
Thiago Henrique <164083408+ThiagoSilvaBr@users.noreply.github.com>
2026-08-29 21:02:14 -0300

    feat: cria dto de saída

--- src/main/java/com/aep/project/dto/EspecieResponse.java
+++ src/main/java/com/aep/project/dto/EspecieResponse.java
@@ -1,4 +1,16 @@
 package com.aep.project.dto;
 
-public record EspecieResponse() {
+import com.aep.project.model.Bioma;
+import com.aep.project.model.Grupo;
+import com.aep.project.model.NivelRisco;
+
+public record EspecieResponse(
+        String id,
+        String nomePopular,
+        String nomeCientifico,
+        Grupo grupo,
+        Bioma bioma,
+        NivelRisco nivelRisco,
+        Integer populacaoEstimada
+) {
 }
