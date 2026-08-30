692ce515ecd02733eefe057e434cabe6467fd526
Thiago Henrique <164083408+ThiagoSilvaBr@users.noreply.github.com>
2026-08-29 21:07:04 -0300

    feat: adiciona mapper para conversão entre DTO e entidade

--- src/main/java/com/aep/project/mapper/EspecieMapper.java
+++ src/main/java/com/aep/project/mapper/EspecieMapper.java
@@ -1,4 +1,34 @@
 package com.aep.project.mapper;
 
+import com.aep.project.dto.EspecieRequest;
+import com.aep.project.dto.EspecieResponse;
+import com.aep.project.model.Especie;
+
 public class EspecieMapper {
+
+    public static Especie mapeandoParaEntidade(EspecieRequest especieRequest){
+        Especie especie = new Especie();
+
+        especie.setNomePopular(especieRequest.nomePopular());
+        especie.setNomeCientifico(especieRequest.nomeCientifico());
+        especie.setGrupo(especieRequest.grupo());
+        especie.setBioma(especieRequest.bioma());
+        especie.setNivelRisco(especieRequest.nivelRisco());
+        especie.setPopulacaoEstimada(especieRequest.populacaoEstimada());
+
+        return especie;
+    }
+
+    public static EspecieResponse mapeandoParaResposta(Especie especie){
+
+        return new EspecieResponse(
+                especie.getId(),
+                especie.getNomePopular(),
+                especie.getNomeCientifico(),
+                especie.getGrupo(),
+                especie.getBioma(),
+                especie.getNivelRisco(),
+                especie.getPopulacaoEstimada()
+        );
+    }
 }
