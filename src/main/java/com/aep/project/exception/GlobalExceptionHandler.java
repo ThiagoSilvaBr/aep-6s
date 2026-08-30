3e0ffe51a1a01893d60d081768c61792672817e6
Thiago Henrique <164083408+ThiagoSilvaBr@users.noreply.github.com>
2026-08-30 19:49:49 -0300

    feat: adiciona tratamento para erros de inserções com enums

--- src/main/java/com/aep/project/exception/GlobalExceptionHandler.java
+++ src/main/java/com/aep/project/exception/GlobalExceptionHandler.java
@@ -2,6 +2,7 @@ package com.aep.project.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -30,4 +31,18 @@ public class GlobalExceptionHandler {
         return ResponseEntity.status(HttpStatus.BAD_REQUEST)
                 .body(erros);
     }
+
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ResponseEntity<Map<String, String>> handleInvalidFormat(HttpMessageNotReadableException exception){
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body(Map.of(
+                        "erro", "Dados Inválidos.", "mensagem", "Verifique se os valores inseridos são compatíveis ao campo.",
+                        "verifique", "grupo, bioma, nivelRisco."
+
+                ));
+
+    }
+
+
 }
