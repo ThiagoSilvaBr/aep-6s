a0db2ab568911ab25f4a62c368690f19d55f80a2
Thiago Henrique <164083408+ThiagoSilvaBr@users.noreply.github.com>
2026-08-30 18:58:38 -0300

    feat: adicionando tratamento para inserções vazias ou nulas

--- src/main/java/com/aep/project/exception/GlobalExceptionHandler.java
+++ src/main/java/com/aep/project/exception/GlobalExceptionHandler.java
@@ -2,9 +2,11 @@ package com.aep.project.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.util.HashMap;
 import java.util.Map;
 
 @RestControllerAdvice
@@ -18,4 +20,14 @@ public class GlobalExceptionHandler {
 
     }
 
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, String>> handleValidationException(MethodArgumentNotValidException exception) {
+        Map<String, String> erros = new HashMap<>();
+
+        exception.getBindingResult().getFieldErrors()
+                .forEach(error -> erros.put(error.getField(), error.getDefaultMessage()));
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                .body(erros);
+    }
 }
